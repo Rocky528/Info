019ae631ed89461a090ad629bffec052b373f85f
Rocky <aokinagashi@gmail.com>
2023-06-08 11:44:35 -0400

    com 11.44am 6.8

--- config/config(db).js
+++ config/config(db).js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://localhost:27017/CandleStick_TradePal").then(() => {
+mongoose.connect("mongodb+srv://CandleStick_Tardepal:bohdanmike@cluster0.0cemq7w.mongodb.net/").then(() => {
     console.log("Database connected!");
 }).catch((err) => {
     console.log(err, " error");
